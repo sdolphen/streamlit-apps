6d0367b39cf0d3ad934a18c234ace88585ec66f8
sdolphen <stijndolphen@gmail.com>
2024-07-01 16:04:21 +0200

    dbgg

--- main.py
+++ main.py
@@ -79,12 +79,12 @@ def display_filtered_columns(df, domain_prefix, display_name):
     st.markdown("<br><br>", unsafe_allow_html=True)
     st.write(f"{display_name} - Consulting Skills")
     st.markdown("<div class='dataframe-container'>", unsafe_allow_html=True)
-    st.dataframe(consulting_df_styled, use_container_width=True, height=100 + len(bu_skills_df) * 20)
+    st.dataframe(consulting_df_styled, use_container_width=True, height=100 + len(consulting_df) * 20)
     st.markdown("</div>", unsafe_allow_html=True)
     
     st.write(f"{display_name} - BU Skills")
     st.markdown("<div class='dataframe-container'>", unsafe_allow_html=True)
-    st.dataframe(bu_skills_df_styled, use_container_width=True, height=500 + len(bu_skills_df) * 20)
+    st.dataframe(bu_skills_df_styled, use_container_width=True, height=475 + len(bu_skills_df) * 20)
     st.markdown("</div>", unsafe_allow_html=True)
 
 # Main function to run the app
